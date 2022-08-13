1ebd8b41125fb256af900ad6b56a28f5d82a45ff
Craig Mullins <craig@mullnet.co.za>
2022-08-13 18:40:10 +0200

    Added Cors

---
+++ README.md
@@ -0,0 +1,3 @@
+# References
+
+[] https://medium.com/litslink/next-js-express-server-pages-routes-and-data-fetching-13bf34cae9c3
--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 3001;
+
+app.use(cors());
 
 const postsList = [
     {
         id: "1",
-        title: "Post 1 Title"
+        title: "Post 1 Title",
+        subTitle: 'aliquam ultrices sagittis',
+        shortText: 'gravida in fermentum et sollicitudin ac orci phasellus',
     },
     {
         id: "2",
-        title: "Post 2 Title"
+        title: "Post 2 Title",
+        subTitle: 'volutpat commodo sed egestas',
+        shortText: 'risus nec feugiat in fermentum posuere urna nec'
     },
+    {
+        id: "3",
+        title: "Post 3 Title",
+        subTitle: 'volutpat commodo sed egestas',
+        shortText: 'risus nec feugiat in fermentum posuere urna nec'
+    }
 ];
 
 app.get('/posts', (request, response) => {
